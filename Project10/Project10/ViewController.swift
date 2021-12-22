062d82e5527456824ff5513e28915b4bbe0b6c1b
phptuts <glaserpower@gmail.com>
2021-12-22 09:25:42 -0800

    Day 42

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Noah Glaser on 12/22/21.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        
+        cell.name.text = "HELLO"
+        return cell
+    }
+
+}
+
