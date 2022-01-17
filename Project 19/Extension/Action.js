e3696187d3aa22dfbc1284165edadaced645ca8e
phptuts <glaserpower@gmail.com>
2022-01-17 12:40:09 -0800

    Day 67

--- Project 19/Extension/Action.js
+++ Project 19/Extension/Action.js
@@ -4,7 +4,8 @@ var Action = function() {}
 Action.prototype = {
     
     run: function(parameters) {
-        
+        parameters.completionFunction({"URL": document.URL, "title": document.title });
+
     },
         
     finalize: function(parameters) {
--- Project 19/Extension/ActionViewController.swift
+++ Project 19/Extension/ActionViewController.swift
@@ -11,16 +11,16 @@ import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
     
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
-                itemProvider.loadItem(forTypeIdentifier: kUTTypeIdentifierKey as String) {
-                    [weak self] (dict, error) in
-                    // do stuff
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    print(javaScriptValues)
+
                 }
             }
         }
